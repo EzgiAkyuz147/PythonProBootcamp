5747a015780548bec11991292313a364537b0016
ezgiakyuz <akyuzezgiee@gmail.com>
2026-08-26 20:41:25 +0300

    Day 9 - Python

---
+++ Day 9/Blind Auction Project/main.py
@@ -0,0 +1,35 @@
+import art
+print(art.logo)
+
+bids ={}
+
+
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+    for key in bidding_dictionary:
+        bid_amount = bidding_dictionary[key]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = key
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name? ")
+    price = int(input("What is your bid? $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no': \n").lower()
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 100)
+
+
+
+
+
+
+
